710d44c882b38fef1cb4dcc698f28f886262ee32
Jorge Gomez <gomezjorge.exequiel@gmail.com>
2016-11-12 22:10:59 -0300

    +Added database.

--- networkMonitor/app.js
+++ networkMonitor/app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var Datastore = require('nedb')
+var db = new Datastore({ filename: 'database/database', autoload: true });
+db.ensureIndex({ fieldName: 'collection', unique: true }, function (err) {
+});
 
 var index = require('./routes/index');
 var users = require('./routes/users');
